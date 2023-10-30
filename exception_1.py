0b727df840c4450a35099c8ff0246c041da64838
ChumaTV <chumatv@gmail.com>
2023-10-30 21:32:33 +0300

    начинаю писать формулу и код

---
+++ exception_1.py
@@ -0,0 +1,6 @@
+print("Программа для решения квадратных уравнений!")
+bad_data = True
+
+while bad_data == True:
+    try:
+        a = int(input("Введите число а:"))
--- kvur.py
+++ kvur.py
@@ -1,1 +1,14 @@
-print("Программа дя решения квадратных уравнений!")
+print("Программа для решения квадратных уравнений!")
+
+ bad_data = True
+
+while bad_data == True:
+ try:
+   a = int(input("Введите число a: "))
+   b = int(input("Введите число b: "))
+   c = int(input("Введите число c: "))
+   bad_data = False
+
+except ValueError:
+   print("Данные не привести к числу")
+
