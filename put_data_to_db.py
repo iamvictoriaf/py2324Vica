ccb9881eb5b2bb0643a3b291836e58e7c315e3fa
vica <council_bracero.0d@icloud.com>
2023-11-09 19:58:21 +0300

    изменила адрес порта

--- put_data_to_db.py
+++ put_data_to_db.py
@@ -1,8 +1,18 @@
 import pymysql
 
-connection = pymysql.connect(host="10.10.101.161", user="pyuser", password="123456", database="test")
+connection = pymysql.connect(host="nadejnei.net", user="student", password="1q2w#E$R", database="test", port=33306)
 
 cursor = connection.cursor()
 
-cursor.execute('insert into telsprav values("vica","frank","f","89116543456")')
+for line in f:
+    line = line.strip()
+    split_line = line.split(':')
+    name = split_line[0]
+    surname = split_line[1]
+    sex = split_line[2]
+    phone = split_line[3]
+    cursor.execute("f'insert into telsprav values("{vica}", "{frank}", "{f}", "{89116543456}")')
+
 connection.commit()
+
+
