8b245759519d31cffef6979bae8eccb3dc081b2d
vica <council_bracero.0d@icloud.com>
2023-11-16 13:47:24 +0300

    изменила файл put_data_to_db.py

--- put_data_to_db.py
+++ put_data_to_db.py
@@ -1,18 +1,10 @@
 import pymysql
 
-f=open("users.txt", "r")
-connection = pymysql.connect(host="nadejnei.net", user="student", password="1q2w#E$R", database="test", port=33306)
+connection = pymysql.connect(host="10.10.101.161", user="pyuser", password="123456", database="test")
+
 cursor = connection.cursor()
 
-for line in f:
-    line = line.strip()
-    split_line = line.split(':')
-    name = split_line[0]
-    surname = split_line[1]
-    sex = split_line[2]
-    phone = split_line[3]
-    cursor.execute(f'insert into telsprav values("{name}", "{surname}", "{sex}", "{phone}")')
-f.close()
+cursor.execute("insert into telsprav values("vica", "frank", "f", "89213456754")")
 
 connection.commit()
 
