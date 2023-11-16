fd27116684389d2a886ba71425fbfdc5f0cc9498
vica <council_bracero.0d@icloud.com>
2023-11-16 13:42:55 +0300

    изменила файл read_data_from_db.py

--- read_data_from_db.py
+++ read_data_from_db.py
@@ -3,12 +3,15 @@ import pymysql
 connection = pymysql.connect(host="nadejnei.net", user="student", password="1q2w#E$R", database="test", port=33306)
 cursor = connection.cursor()
 
-for line in f:
-    line = line.strip()
-    split_line = line.split(':')
-    name = split_line[0]
-    surname = split_line[1]
-    sex = split_line[2]
-    phone = split_line[3]
-    cursor.execute(f'insert into telsprav values("{name}", "{surname}", "{sex}", "{phone}")')
-f.close()
+cursor.execute=("select * from telsprav")
+
+data = cursor.fetchall()
+
+for elem in data:
+    print(elem)
+    name = elem[0]
+    surname = elem[1]
+    sex = elem[2]
+    phone = elem[3]
+
+    print(name, phone)
