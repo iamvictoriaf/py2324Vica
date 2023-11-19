6099b1991e8a3f05da5068a35048740e83df251b
vica <council_bracero.0d@icloud.com>
2023-11-19 16:37:27 +0300

    создала файт d1

---
+++ d1.py
@@ -0,0 +1,19 @@
+d1 = {'elizaveta':'89213651412','julia':'89773215678','vladimir':'89345672134'}
+
+d2 = {
+    'igor':'89324564376',
+    'svetlana':'9874563245',
+    'vera':'89623773123'
+}
+
+try:
+    name = input('Введите имя: ')
+    print(f'Телефон абонента {name} - {d1[name]}')
+except KeyError:
+    print(f'Телефон абонента {name} - не найден')
+#d1.update(d2)
+#print(d1)
+
+#'print(d1.items())
+#print(d1.keys())
+#print(d1.values())'
