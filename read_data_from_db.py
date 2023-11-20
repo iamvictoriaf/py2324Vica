7963b7d36179e8ce7b3b4be9edd7a3bb6d987690
vica <council_bracero.0d@icloud.com>
2023-11-20 19:26:31 +0300

    пишет "Абонент не найден" даже если он есть в базе

--- read_data_from_db.py
+++ read_data_from_db.py
@@ -1,18 +1,22 @@
 import pymysql
 
-connection = pymysql.connect(host="nadejnei.net", user="student", password="1q2w#E$R", database="test", port=33306)
+connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
 cursor = connection.cursor()
-
-cursor.execute=("select name, phone from telsprav")
+cursor.execute('select name, phone  from telsprav')
 
 data = cursor.fetchall()
-print(data)
+
 telsprav={}
 for element in data:
-    print(element)
+    #print(element)
     #telsprav[]
     name = element[0]
     phone = element[1]
     telsprav[name]=phone
 
 print(telsprav)
+
+while True:
+    username = input("Введите имя абонента: ")
+    phone = telsprav.get(username, "не найден")
+    print(f'Телефон абонента {username} - {phone}')
