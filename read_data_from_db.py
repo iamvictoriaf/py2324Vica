ee3e7cf6c1f97e356213479067af496f33238e4d
vica <council_bracero.0d@icloud.com>
2023-11-20 19:11:03 +0300

    переписала код в файле read_data_from_db.py

--- read_data_from_db.py
+++ read_data_from_db.py
@@ -3,15 +3,16 @@ import pymysql
 connection = pymysql.connect(host="nadejnei.net", user="student", password="1q2w#E$R", database="test", port=33306)
 cursor = connection.cursor()
 
-cursor.execute=("select * from telsprav")
+cursor.execute=("select name, phone from telsprav")
 
 data = cursor.fetchall()
+print(data)
+telsprav={}
+for element in data:
+    print(element)
+    #telsprav[]
+    name = element[0]
+    phone = element[1]
+    telsprav[name]=phone
 
-for elem in data:
-    print(elem)
-    name = elem[0]
-    surname = elem[1]
-    sex = elem[2]
-    phone = elem[3]
-
-    print(name, phone)
+print(telsprav)
