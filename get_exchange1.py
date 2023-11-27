331451a3147d60941df46c6d8d10de8851ab81ed
vica <council_bracero.0d@icloud.com>
2023-11-27 19:43:38 +0300

    Добавила valutes_raw_dict

--- get_exchange1.py
+++ get_exchange1.py
@@ -2,14 +2,20 @@ import requests
 def get_data_from_cb(site):
     result = requests.get(site)
     valites = result.json()
-    print()
+    valutes_raw_dict = valites["Valute"]
+    #print(valutes_raw_dict)
+    for val in valutes_raw_dict:
+        print("----------------------")
+        print(val)
+        print(valutes_raw_dict[val]["Value"])
+        print("----------------------")
     pass
+
 def put_data_to_db():
     pass
 
 if __name__ == "__main__":
     cb_site = "https://www.cbr-xml-daily.ru/daily_json.js"
-
     data = get_data_from_cb(cb_site)
 
     #put_result = put_data_to_db(data)
